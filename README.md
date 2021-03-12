6dd547f67e4b11f848334c39f0bbd14c97a46ca2
Nicolò Giuliani <info@ngiuliani.com>
2021-03-12 05:52:35 +0100

    Development details page.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 A new Flutter application. Fudeo Start final project.
 
 ## Project Notes
+
+- Lessons:
+    - Lesson 3.4: https://bit.ly/2NbnVzh
+        - Argument: **Sviluppo App - Parte 1**
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(App());
@@ -40,6 +41,7 @@ class HomePage extends StatelessWidget {
         currentIndex: 0,
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.black45,
+        elevation: 0,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.star_rounded),
@@ -94,46 +96,56 @@ class LastHourPage extends StatelessWidget {
     );
   }
 
-  Widget buildArticle() {
-    return Row(
-      children: [
-        /// La [Column()], presente all'interno di questo [Expanded()], senza
-        /// questo refactoring non si sarebbe potuta espandere correttamente.
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Casa Bianca, Tarrant come Pelosi e Cortez",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Text(
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                maxLines: 3,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.black45,
-                ),
-              ),
-            ],
+  Widget buildArticle(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ArticleDetailPage(),
           ),
-        ),
-        Container(
-          height: 80,
-          width: 80,
-          decoration: BoxDecoration(
-            color: Colors.grey.shade500,
-            borderRadius: BorderRadius.circular(12),
+        );
+      },
+      child: Row(
+        children: [
+          /// La [Column()], presente all'interno di questo [Expanded()], senza
+          /// questo refactoring non si sarebbe potuta espandere correttamente.
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Casa Bianca, Tarrant come Pelosi e Cortez",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black45,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+          Container(
+            height: 80,
+            width: 80,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade500,
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -152,7 +164,7 @@ class LastHourPage extends StatelessWidget {
         else
           return Padding(
             padding: EdgeInsets.only(top: 32),
-            child: buildArticle(),
+            child: buildArticle(context),
           );
       },
     );
@@ -206,6 +218,67 @@ class OldPage extends StatelessWidget {
             ],
           );
         },
+      ),
+    );
+  }
+}
+
+class ArticleDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_rounded),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.link_rounded),
+            onPressed: () {
+              launch("https://www.google.com");
+            },
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade500,
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              "Casa Bianca, Tarrant come Pelosi e Cortez",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black45,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
