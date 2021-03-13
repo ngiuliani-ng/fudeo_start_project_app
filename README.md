03117d53450512721fb3b3c9def86fe82ba23444
Nicolò Giuliani <info@ngiuliani.com>
2021-03-13 19:33:21 +0100

    Update.

--- README.md
+++ README.md
@@ -9,9 +9,7 @@ A new Flutter application. Fudeo Start final project.
         - Argument: **Sviluppo App - Parte 1**
     - Lesson 3.5: https://bit.ly/3bJdQ6c
         - Argument: **Sviluppo App - Parte 2**
-
-- Screenshot App:
-    - Trend: Screenshot\Screenshot_1615657820.png
-        - Article Detail: Screenshot\Screenshot_1615657823.png
-    - All: Screenshot\Screenshot_1615657829.png
-        - Article Detail: Screenshot\Screenshot_1615657834.png
+            - Screenshot App: Screenshot\Screenshot_1615657820.png - TrendPage
+            - Screenshot App: Screenshot\Screenshot_1615657823.png - ArticleDetailPage from TrendPage
+            - Screenshot App: Screenshot\Screenshot_1615657829.png - AllPage
+            - Screenshot App: Screenshot\Screenshot_1615657834.png - ArticleDetailPage from AllPage
--- lib/providers/news_api.dart
+++ lib/providers/news_api.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:fudeo_start_project_app/providers/config.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:fudeo_start_project_app/models/article.dart';
 
 /// API: https://newsapi.org/docs/endpoints/top-headlines
 Future<List<Article>> topHeadlines() async {
-  final response = await http.get(Uri.parse("https://newsapi.org/v2/top-headlines?sources=ansa&apiKey=6a363f5724254353b5a4d5632bd23157"));
+  final apiKey = Config().apiKey;
+
+  final response = await http.get(Uri.parse("https://newsapi.org/v2/top-headlines?sources=ansa&apiKey=$apiKey"));
   List articles = json.decode(response.body)["articles"];
 
   return articles.map((data) => Article.fromData(data)).toList();
@@ -14,7 +17,9 @@ Future<List<Article>> topHeadlines() async {
 
 /// API: https://newsapi.org/docs/endpoints/everything
 Future<List<Article>> everything() async {
-  final response = await http.get(Uri.parse("https://newsapi.org/v2/everything?sources=ansa&apiKey=6a363f5724254353b5a4d5632bd23157"));
+  final apiKey = Config().apiKey;
+
+  final response = await http.get(Uri.parse("https://newsapi.org/v2/everything?sources=ansa&apiKey=$apiKey"));
   List articles = json.decode(response.body)["articles"];
 
   return articles.map((data) => Article.fromData(data)).toList();
